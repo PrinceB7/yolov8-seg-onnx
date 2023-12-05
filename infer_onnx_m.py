f0ce181a3ee3fe06e27c83cbc3e9cf599e0f6d8e
PrinceB7 <bunyod.ibrokhimov@gmail.com>
2023-12-05 22:43:19 +0900

    segment

--- infer_onnx_m.py
+++ infer_onnx_m.py
@@ -4,13 +4,12 @@ from yoloseg_onnx.yoloseg import YOLOSeg
 
 if __name__ == '__main__':
     
-    parser = argparse.ArgumentParser(description='Yolov8 onnx inference')
+    parser = argparse.ArgumentParser(description='Yolov8-seg onnx inference')
     parser.add_argument('-c', '--checkpoint_path', default="weights/yolov8n-seg-v1.onnx",
                         help='Dataset version with choices')
     parser.add_argument('-i','--image_path', default="data/5.png",
                         help='List of model IDs with default values')
     args = parser.parse_args()
-    
     
     yoloseg = YOLOSeg(args.checkpoint_path, conf_thres=0.5, iou_thres=0.3)
 
